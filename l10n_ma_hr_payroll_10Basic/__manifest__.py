22f6a52c8cac2789552f82c5032cb8532b5a05e6
saidhij <saidhij@users.noreply.github.com>
2022-06-12 10:50:10 +0000

    Update __manifest__.py
    email

--- l10n_ma_hr_payroll_10Basic/__manifest__.py
+++ l10n_ma_hr_payroll_10Basic/__manifest__.py
@@ -21,8 +21,8 @@
 {
     'name': 'Morocco-payroll Basic',
     'category': 'Payroll',
-    'author': 'paiesoft.pro',
-    'website': 'http://paiesoft.pro',
+    'author': 'soffordoc@gmail.com',
+    'website': 'http://paiesoft.casa',
     'version': '1.12Basic',
     'depends': ['hr_payroll'],
     
