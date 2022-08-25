17097736ea48fd45db7eade20553ff2b750fd4f4
saidhij <saidhij@users.noreply.github.com>
2022-08-25 15:21:18 +0000

    Add files via upload
    new

---
+++ hr_payroll_ma_pro/models/hr_payroll_run_lot_payslips.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from collections import defaultdict
+from datetime import datetime, date, time
+import pytz
+
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+
+class HrPayslipEmployees(models.TransientModel):
+    _inherit = 'hr.payslip.employees'
+    _description = 'Generate payslips for all selected employees'
+
+
+
+    employees_ids = fields.Many2many('hr.employee', 'hr_employee_group_rel', 'payslip_id', 'employee_id', 'Employees',
+                                    default=lambda self: self._get_employees(), required=True)
+    structures_id = fields.Many2one('hr.payroll.structure', string='Structure de salaire')
